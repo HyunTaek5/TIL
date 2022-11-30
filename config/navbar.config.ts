803fcf33b2b33551fcd995328482ec72d3a31887
HyunTaek Oh <oht222@naver.com>
2022-12-01 02:55:47 +0900

    add: 상단 내비게이션바 수정

--- config/navbar.config.ts
+++ config/navbar.config.ts
@@ -1,38 +1,44 @@
 const navbar = {
-    title: 'HyunTaek5',
-    logo: {
-        alt: 'HyunTaek Oh Logo',
-        src: 'img/favicon.svg'
-    },
-    items: [
-        // {
-        //     type: 'doc',
-        //     docId: 'Hey',
-        //     position: 'left',
-        //     label: 'Daily Log'
-        // },
-        {
-            to: "/w/dev-log",
-            label: "Dev Logs",
-            position: "left",
-        },
-        {
-            href: 'https://github.com/HyunTaek5',
-            position: 'right',
-            className: 'navbar-github-link',
-            'aria-label': 'GitHub repository',
-        },
-        {
-            href: 'https://linkedin.com/in/dylanoh',
-            position: 'right',
-            className: 'navbar-linkedin-link',
-            'aria-label': 'LinkedIn Account',
-        },
-        {
-            type: 'localeDropdown',
-            position: 'right',
-        },
-    ]
-}
+  title: '8:20 Dev Log',
+  logo: {
+    alt: 'HyunTaek Oh Logo',
+    src: 'img/favicon.png',
+  },
 
-export default navbar
+  items: [
+    // {
+    //     type: 'doc',
+    //     docId: 'Hey',
+    //     position: 'left',
+    //     label: 'Daily Log'
+    // },
+    {
+      to: '/w/dev-log',
+      label: 'Dev Log',
+      position: 'left',
+    },
+    {
+      to: '/w/projects',
+      label: 'Projects',
+      position: 'left',
+    },
+    {
+      href: 'https://github.com/HyunTaek5',
+      position: 'right',
+      className: 'navbar-github-link',
+      'aria-label': 'GitHub repository',
+    },
+    {
+      href: 'https://linkedin.com/in/dylanoh',
+      position: 'right',
+      className: 'navbar-linkedin-link',
+      'aria-label': 'LinkedIn Account',
+    },
+    {
+      type: 'localeDropdown',
+      position: 'right',
+    },
+  ],
+};
+
+export default navbar;
