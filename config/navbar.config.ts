05019cf283b4cf5bf8f68d033c832dd79b61f4fe
HyunTaek Oh <oht222@naver.com>
2023-09-12 00:22:31 +0900

    fix: 세팅 수정 (#9)
    * fix: lint 세팅 수정 및 baseUrl 변경
    
    * update: .gitignore에 .obsidian 추가
    
    * refact: blog 디렉토리 -> log로 수정
    
    * chore: navbar 수정
    
    * fix: 라우팅 이슈 수정
    
    * add: TIL 자동 커밋 shell 추가

--- config/navbar.config.ts
+++ config/navbar.config.ts
@@ -1,30 +1,19 @@
 const navbar = {
-  title: '8:20 Dev Log',
+  title: '8:20 TIL',
   logo: {
     alt: 'HyunTaek Oh Logo',
     src: 'img/favicon.png',
   },
 
   items: [
-    // {
-    //     type: 'doc',
-    //     docId: 'Hey',
-    //     position: 'left',
-    //     label: 'Daily Log'
-    // },
     {
-      to: '/about',
+      to: 'https://hyuntaek5.github.io',
       label: 'About',
       position: 'left',
     },
     {
-      to: '/w/dev-log',
+      to: 'https://eight20.tistory.com',
       label: 'Dev Log',
-      position: 'left',
-    },
-    {
-      to: '/w/projects',
-      label: 'Projects',
       position: 'left',
     },
     {
